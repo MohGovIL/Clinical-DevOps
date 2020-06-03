3ee3c5bec221f4ae5a61573c8995744a0e50b078
shaharzi <shaharzi@matrix.co.il>
2020-06-03 21:18:55 +0300

    temporary hotfix in image building

--- images/server/build.sh
+++ images/server/build.sh
@@ -4,4 +4,4 @@ FULL_PATH=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
 
 . $FULL_PATH/image.cfg
 
-docker build --tag israelimoh/clinikal:$VERTICAL-$VERTICAL_VERSION-$ENVIRONMENT $FULL_PATH/$VERTICAL/$VERTICAL_VERSION/$ENVIRONMENT/.
+docker build --no-cache --tag israelimoh/clinikal:$VERTICAL-$VERTICAL_VERSION-$ENVIRONMENT $FULL_PATH/$VERTICAL/$VERTICAL_VERSION/$ENVIRONMENT/.

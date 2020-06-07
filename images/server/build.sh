31a2dab4729c14ddf370ce710924029f81847a4f
shaharzi <shaharzi@matrix.co.il>
2020-06-07 16:53:52 +0300

    test image mods

--- images/server/build.sh
+++ images/server/build.sh
@@ -4,4 +4,7 @@ FULL_PATH=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
 
 . $FULL_PATH/image.cfg
 
-docker build --no-cache --tag israelimoh/clinikal:$VERTICAL-$VERTICAL_VERSION-$ENVIRONMENT $FULL_PATH/$VERTICAL/$VERTICAL_VERSION/$ENVIRONMENT/.
+docker build \
+    --tag israelimoh/clinikal:$VERTICAL-$VERTICAL_VERSION-$ENVIRONMENT \
+    --build-arg CACHEBUST=$(date +%s) \
+    $FULL_PATH/$VERTICAL/$VERTICAL_VERSION/$ENVIRONMENT/.
